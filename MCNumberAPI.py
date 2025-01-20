00bc22ef282af4ad43dac86cb9d43a1e255ec8da
Shaamil Karim <40500380+shaamil101@users.noreply.github.com>
2025-01-20 15:53:12 -0500

    Update MCNumberAPI.py

--- MCNumberAPI.py
+++ MCNumberAPI.py
@@ -3,7 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-# Your FMCSA API key
 API_KEY = "cdc33e44d693a3a58451898d4ec9df862c65b954"
 
 @app.route('/', methods=['GET'])
@@ -14,13 +13,10 @@ def allowed_to_operate(reference_number=None):
     """
     try:
         reference_number = request.args.get('mc_number')
-        # Build the FMCSA API URL
         url = f"https://mobile.fmcsa.dot.gov/qc/services/carriers/{reference_number}?webKey={API_KEY}"
         
-        # Make the API request
         response = requests.get(url)
         
-        # Check for successful response
         if response.status_code == 200:
             data = response.json()
             # Extract the 'allowedToOperate' field
@@ -45,7 +41,6 @@ def allowed_to_operate(reference_number=None):
             "details": str(e)
         }), 500
 
-# Run the application
 import os
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
